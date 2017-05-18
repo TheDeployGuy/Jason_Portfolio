7e7944974da3a5bb9d75a789da87b13497e05229
Jason Lloyd <jason.lloyddev@gmail.com>
2017-05-18 21:00:38 +0100

    Add technologies images for use in future, change color scheme for testing

--- src/components/TechnologySection.js
+++ src/components/TechnologySection.js
@@ -7,21 +7,35 @@ class TechnologySection extends Component {
             <div className="header-content" id="TECHNOLOGY">
                 <h1>Technologies I work with</h1>
                 <hr/>
-                <div className="container">
-                  {/*I could make the projects clickable and they would show the projects I done in that language...*/}
-                  {/*MongoDB*/}
-                  {/*ExpressJS*/}
-                  {/*Angular 2*/}
-                  {/*NodeJS*/}
-                  {/*React*/}
-                  {/*Python*/}
-                  {/*Bash*/}
+                  <div className="row">
+                      <div className="col-md-4">
+                            <img alt="mean_stack" src="img/mean.png" className="techImage" />
+                            
+                            {/*<img alt="expressjs" src="img/expressjs.png" className="techImage" />
+                            <img alt="angular_2" src="img/angular_2.jpg" className="techImage" />
+                            <img alt="nodejs" src="img/nodejs_2.png" className="techImage" />*/}
+                      </div>
+                      <div className="col-md-4">
+                            <img alt="reactjs" src="img/reactjs.png" className="techImage" />
+                      </div>
+                      <div className="col-md-4">
+                        <img alt="python" src="img/python.png" className="techImage" />
+                      </div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-12">
+                        {/*Python*/}
+                        
+                        {/*Bash*/}
+                      </div>
+                  </div>
                 </div>
-                <div id="moveForward">
-                    <p><small>What I build with these</small></p>
-                    <a href="#PROJECTS"><i id="downArrow2" className="fa fa-chevron-circle-down fa-3x" aria-hidden="true"></i></a>
+                <div className="container text-center">
+                    <div id="moveForward ">
+                        <p><small>What I build with these</small></p>
+                        <a href="#PROJECTS"><i id="downArrow2" className="fa fa-chevron-circle-down fa-3x" aria-hidden="true"></i></a>
+                    </div>
                 </div>
-            </div>
         </section>
    )
   }
