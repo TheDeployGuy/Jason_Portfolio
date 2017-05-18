c5f2a7e44b6acf0d283039d437191777ea8472c6
Jason Lloyd <jason.lloyddev@gmail.com>
2017-05-18 22:57:35 +0100

    Add paragraphs to technologies and improve head section look

--- src/components/TechnologySection.js
+++ src/components/TechnologySection.js
@@ -9,30 +9,30 @@ class TechnologySection extends Component {
                 <hr/>
                   <div className="row">
                       <div className="col-md-4">
-                            <img alt="mean_stack" src="img/mean.png" className="techImage" />
-                            
-                            {/*<img alt="expressjs" src="img/expressjs.png" className="techImage" />
-                            <img alt="angular_2" src="img/angular_2.jpg" className="techImage" />
-                            <img alt="nodejs" src="img/nodejs_2.png" className="techImage" />*/}
+                        <img alt="mean_stack" src="img/mean.png" className="techImage" />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed dictum sem, quis consequat enim. Ut quis nulla tempor, gravida neque in, pharetra mi.</p>
+                        {/*Add details info here*/}
                       </div>
                       <div className="col-md-4">
-                            <img alt="reactjs" src="img/reactjs.png" className="techImage" />
+                        <img alt="reactjs" src="img/reactjs.png" className="techImage" />
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent diam libero, accumsan ac nisl at, lacinia pulvinar enim. Praesent viverra, massa vel euismod tristique.</p>
+                        {/*Add details info here*/}
                       </div>
                       <div className="col-md-4">
                         <img alt="python" src="img/python.png" className="techImage" />
+                        {/*Add details info here*/}
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse imperdiet placerat nisi id egestas. Nunc sed arcu ut massa vestibulum maximus faucibus id est.</p>
                       </div>
                   </div>
                   <div className="row">
                       <div className="col-md-12">
-                        {/*Python*/}
-                        
-                        {/*Bash*/}
+
                       </div>
                   </div>
                 </div>
                 <div className="container text-center">
                     <div id="moveForward ">
-                        <p><small>What I build with these</small></p>
+                        <p><small>Projects built with these</small></p>
                         <a href="#PROJECTS"><i id="downArrow2" className="fa fa-chevron-circle-down fa-3x" aria-hidden="true"></i></a>
                     </div>
                 </div>
