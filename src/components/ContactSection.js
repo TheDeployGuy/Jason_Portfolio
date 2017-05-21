288da7b3a4f36235071801ef54bf61834d95072e
Jason Lloyd <jason.lloyddev@gmail.com>
2017-05-21 17:26:28 +0100

    Cleanup contact section, add email and other contact links

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -9,18 +9,25 @@ class ContactSection extends Component {
             <h1>Reaching out</h1>
             <hr className="headerHR" />
             <div className="row">
-                <div className="col-md-offset-2 col-md-8">
-									<img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/026/0f8/2a60dd4.jpg" className="roundImage img-circle" />
-
-                </div>
+							  <img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/026/0f8/2a60dd4.jpg" className="roundImage img-circle" />
             </div>
 						<div className="row">
-							<div className="col-md-offset-2 col-md-8">
-									<a className="btn btn-default btn-lg" href="https://www.linkedin.com/in/jason-lloyd/" id="linkedinButton"><i className="fa fa-linkedin fa-2x"></i></a>
-									<a className="btn btn-default btn-lg" href="http://github.com/thedeployguy" id="linkedinButton"><i className="fa fa-envelope fa-2x"></i></a>
-									<a className="btn btn-default btn-lg" href="http://github.com/thedeployguy" id="githubButton"><i className="fa fa-github fa-2x"></i></a>
-							</div>
+                <div className="col-md-offset-4 col-md-1">
+                    <a className="btn btn-default btn-lg" href="mailto:jaslloyd57@gmail.com" id="linkedinButton"><i className="fa fa-envelope fa-2x"></i></a>
+                  {/*https://bootsnipp.com/snippets/featured/quick-contact*/}
+                </div>
+                <div className="col-md-4 text-left">
+                  <h2>jaslloyd@gmail.com</h2>
+                </div>
 						</div>
+            <div className="row">
+                <div className="col-md-offset-4 col-md-1">
+                    <a className="btn btn-default btn-lg" href="https://www.linkedin.com/in/jason-lloyd/" id="linkedinButton"><i className="fa fa-linkedin fa-2x"></i></a>
+                </div>
+                <div className="col-md-4 text-left">
+                  <h2>Jason-Lloyd</h2>
+                </div>
+            </div>
 				</div>
       <footer className="footer">
           <div className="container text-right">
--- src/components/HeadSection.js
+++ src/components/HeadSection.js
@@ -11,7 +11,7 @@ class HeadSection extends Component {
                         <hr className="headerHR" />
                         <p className="headerText">My name is Jason, I have been working for 3+ years as an Automation Engineer. I am also an inspiring Full stack developer mainly specializing in the MEAN stack. Here you will find personal projects and client projects. If you think I would be a good fit for your project</p>
                         <a href="#CONTACT" className="btn btn-lg btn-info">I need a website</a>
-                        <button type="button" className="btn btn-lg btn-success">Contact me</button>
+                        <a href="#CONTACT" className="btn btn-lg btn-success">Contact me</a>
                         <div id="moveForward">
                             <p><small>My Projects</small></p>
                             <a href="#PROJECTS"><i id="downArrow" className="fa fa-chevron-circle-down fa-3x" aria-hidden="true"></i></a>
