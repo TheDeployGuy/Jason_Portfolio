bd877cd89a8f342b638204bf009c056a9504904d
Jason Lloyd <jason.lloyddev@gmail.com>
2017-05-21 12:22:16 +0100

    Add footer to give the contact page more space

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -4,47 +4,29 @@ class ContactSection extends Component {
   render() {
     return (
     <section>
-      <div className="text-center" id="CONTACT">
-				<div className="container">
-            <h1>Contact me</h1>
+      <div className="" id="CONTACT">
+				<div className="container text-center">
+            <h1>Reaching out</h1>
             <hr className="headerHR" />
             <div className="row">
-                <div className="col-md-offset-3 col-md-6">
-                    <form className="form-horizontal" role="form" method="post" action="index.php">
-	                    <div className="form-group">
-		                    <label htmlFor="name" className="col-sm-2 control-label">Name</label>
-		                    <div className="col-sm-10">
-			                    <input type="text" className="form-control" id="name" name="name" placeholder="First & Last Name" value="" />
-		                    </div>
-	                    </div>
-	                    <div className="form-group">
-		                    <label htmlFor="email" className="col-sm-2 control-label">Email</label>
-		                    <div className="col-sm-10">
-			                    <input type="email" className="form-control" id="email" name="email" placeholder="example@domain.com" value="" />
-		                    </div>
-	                    </div>
-	                    <div className="form-group">
-		                    <label htmlFor="message" className="col-sm-2 control-label">Message</label>
-		                    <div className="col-sm-10">
-			                    <textarea className="form-control" rows="4" name="message"></textarea>
-		                    </div>
-	                    </div>
-	                    <div className="form-group">
-		                    <div className="col-sm-offset-10 col-md-2">
-			                    <input id="submit" name="submit" type="submit" value="Send" className="btn btn-primary" />
-		                    </div>
-	                    </div>
-	                    <div className="form-group">
-		                    <div className="col-sm-10 col-sm-offset-2">
-			                    
-		                    </div>
-	                    </div>
-                    </form>
+                <div className="col-md-offset-2 col-md-8">
+									<img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/026/0f8/2a60dd4.jpg" className="roundImage img-circle" />
+
                 </div>
             </div>
-						<a className="btn btn-default btn-lg btn-git" href="https://www.linkedin.com/in/jason-lloyd/" id="linkedinButton"><i className="fa fa-linkedin fa-2x"></i></a>
-						<a className="btn btn-default btn-lg btn-git" href="http://github.com/thedeployguy" id="githubButton"><i className="fa fa-github fa-2x"></i></a>
-					</div>
+						<div className="row">
+							<div className="col-md-offset-2 col-md-8">
+									<a className="btn btn-default btn-lg" href="https://www.linkedin.com/in/jason-lloyd/" id="linkedinButton"><i className="fa fa-linkedin fa-2x"></i></a>
+									<a className="btn btn-default btn-lg" href="http://github.com/thedeployguy" id="linkedinButton"><i className="fa fa-envelope fa-2x"></i></a>
+									<a className="btn btn-default btn-lg" href="http://github.com/thedeployguy" id="githubButton"><i className="fa fa-github fa-2x"></i></a>
+							</div>
+						</div>
+				</div>
+      <footer className="footer">
+          <div className="container text-right">
+            Designed & Developed by Jason Lloyd
+          </div>
+      </footer>
      </div>
    </section>
     );
--- src/components/HeadSection.js
+++ src/components/HeadSection.js
@@ -9,8 +9,7 @@ class HeadSection extends Component {
                     <div className="col-md-offset-3 col-md-6">
                         <h1>My Portfolio</h1>
                         <hr className="headerHR" />
-                        {/*<img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/005/026/0f8/2a60dd4.jpg" className="roundImage img-circle" />*/}
-                        <p className="text-left">My name is Jason, I have been working for 3+ years as an Automation Engineer. I am also an inspiring Full stack developer mainly specializing in the MEAN stack.Here you will find personal projects and client projects. If you think I would be a good fit for your project</p>
+                        <p className="headerText">My name is Jason, I have been working for 3+ years as an Automation Engineer. I am also an inspiring Full stack developer mainly specializing in the MEAN stack. Here you will find personal projects and client projects. If you think I would be a good fit for your project</p>
                         <a href="#CONTACT" className="btn btn-lg btn-info">I need a website</a>
                         <button type="button" className="btn btn-lg btn-success">Contact me</button>
                         <div id="moveForward">
