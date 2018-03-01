95cc869bf2fafb3e17c5095008579b1a19b4542b
Jason Lloyd <jason.lloyddev@gmail.com>
2018-03-01 18:26:00 +0000

    Conver to const pure functions

--- src/components/HeadSection.jsx
+++ src/components/HeadSection.jsx
@@ -1,30 +1,28 @@
 import React from 'react';
 
-function HeadSection() {
-  return (
-    <header id="HOME">
-      <div className="header-content">
-        <div className="row">
-          <div className="m-auto col-md-6">
-            <h1>My Portfolio</h1>
-            <hr className="headerHR" />
-            <img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAQDGAAwAAQAAAAAAAAyaAAAAJGE0MmRlNDMzLTc0YTYtNDZlOS05MzQ5LWEzYmIxOTE4NmVhZg.jpg" className="roundImage img-circle" />
-            <p className="mb-4">
-              My name is Jason, I am currently working as a Senior Software Enginner at IBM building Full stack applications. I have worked with clients creating projects with Angular 2/4, React and VueJS. Here you a find collection of personal and client projects.
-            </p>
-            <a href="[insert_later]" target="_blank" className="btn btn-lg btn-success btn-outline-success">Hire me</a>
-            <a href="#CONTACT" className="btn btn-lg btn-info btn-outline-info">Need a website?</a>
-            <div id="moveForward">
-              {/* // eslint-disable-next-line */}
-              <a href="#PROJECTS">
-                <i id="downArrow" role="link" className="fa fa-chevron-down fa-2x animated infinite rubberBand" aria-hidden="true" />
-              </a>
-            </div>
+const HeadSection = () => (
+  <header id="HOME">
+    <div className="header-content">
+      <div className="row">
+        <div className="m-auto col-md-6">
+          <h1>My Portfolio</h1>
+          <hr className="headerHR" />
+          <img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAQDGAAwAAQAAAAAAAAyaAAAAJGE0MmRlNDMzLTc0YTYtNDZlOS05MzQ5LWEzYmIxOTE4NmVhZg.jpg" className="roundImage img-circle" />
+          <p className="mb-4">
+          My name is Jason, I am currently working as a Senior Software Enginner at IBM building Full stack applications. I have worked with clients creating projects with Angular 2/4, React and VueJS. Here you a find collection of personal and client projects.
+          </p>
+          <a href="[insert_later]" target="_blank" className="btn btn-lg btn-success btn-outline-success">Hire me</a>
+          <a href="#CONTACT" className="btn btn-lg btn-info btn-outline-info">Need a website?</a>
+          <div id="moveForward">
+            {/* // eslint-disable-next-line */}
+            <a href="#PROJECTS">
+              <i id="downArrow" role="link" className="fa fa-chevron-down fa-2x animated infinite rubberBand" aria-hidden="true" />
+            </a>
           </div>
         </div>
       </div>
-    </header>
-  );
-}
+    </div>
+  </header>
+);
 
 export default HeadSection;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import ProjectItem from './ProjectItem';
 
-// class ProjectsSection extends Component {
-function ProjectsSection({ projects }) {
-  // For each project we map though each element and create a ProjectItem component
-  const projectItems = projects.map(project => (
-    // onDelete has been set as a property that will call a function with its ID, in this case it uses an inline arrow function to create the onDeleteProps (could easily repalce this with a function)
-    <ProjectItem key={project.projectTitle} project={project} />
-  ));
-
-  return (
-    <section>
-      <div className="header-content" id="PROJECTS">
-        <h1>Projects</h1>
-        <hr className="headerHR" />
-        <div className="container">
-          {projectItems}
-        </div>
+const ProjectsSection = ({ projects }) => (
+  <section>
+    <div className="header-content" id="PROJECTS">
+      <h1>Projects</h1>
+      <hr className="headerHR pt-5" />
+      <div className="container">
+        { projects.map(project => <ProjectItem key={project.projectTitle} project={project} />)}
       </div>
-    </section>
-  );
-}
+    </div>
+  </section>
+);
 
 export default ProjectsSection;
