13b135e8c0b19497b8253b810f514eb7fc9b2d26
Jason Lloyd <jason.lloyddev@gmail.com>
2018-03-08 18:04:15 +0000

    Improve modal look

--- src/components/ContactModal.jsx
+++ src/components/ContactModal.jsx
@@ -38,32 +38,23 @@ class ContactSection extends Component {
             </div>
             <div className="modal-body">
               <form onSubmit={e => this.handleSubmit(e)}>
-                <div className="form-group row">
-                  <label htmlFor="name" className="col-sm-2">Name:</label>
-                  <div className="col-sm-10">
-                    <input type="name" name="name" className="form-control" id="name" onChange={e => this.handleChange(e)} required />
-                  </div>
+                <div className="form-group">
+                  <label htmlFor="recipient-name" className="col-form-label">Name:</label>
+                  <input type="name" name="name" className="form-control" id="name" onChange={e => this.handleChange(e)} required />
                 </div>
-                <div className="form-group row">
-                  <label htmlFor="email" className="col-sm-2">Email:</label>
-                  <div className="col-sm-10">
-                    <input type="email" name="email" className="form-control" id="email" onChange={e => this.handleChange(e)} required />
-                  </div>
+                <div className="form-group">
+                  <label htmlFor="message-text" className="col-form-label">Email:</label>
+                  <input type="email" name="email" className="form-control" id="email" onChange={e => this.handleChange(e)} required />
                 </div>
-                <div className="form-group row">
-                  <label htmlFor="message" className="col-sm-2">Message:</label>
-                  <div className="col-sm-10">
-                    <textarea rows="5" name="message" className="form-control" id="message" onChange={e => this.handleChange(e)} required />
-                  </div>
-                </div>
-                <div className="form-group text-right">
-                  <button type="submit" className="btn btn-success">Submit</button>
+                <div className="form-group">
+                  <label htmlFor="message-text" className="col-form-label">Message:</label>
+                  <textarea rows="5" name="message" className="form-control" id="message" onChange={e => this.handleChange(e)} required />
                 </div>
               </form>
             </div>
             <div className="modal-footer">
               <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="button" className="btn btn-primary">Save changes</button>
+              <button type="submit" className="btn btn-success">Submit</button>
             </div>
           </div>
         </div>
