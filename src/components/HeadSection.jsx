f4f57be3a95d5cfc31f5af0802551e00bc483cf1
Jason Lloyd <jason.lloyddev@gmail.com>
2018-03-17 20:24:18 +0000

    Change color of links in header for better visualization

--- src/components/HeadSection.jsx
+++ src/components/HeadSection.jsx
@@ -6,8 +6,8 @@ const HeadSection = () => (
     <div className="header-content">
       <div className="row justify-content-end">
         <div className="col-2">
-          <a className="btn btn-default" href="mailto:jaslloyd57@gmail.com"><i className="fa fa-envelope fa-2x" /></a>
-          <a className="btn btn-default" href="https://www.linkedin.com/in/jason-lloyd/"><i className="fa fa-linkedin fa-2x" /></a>
+          <a id="email-button" className="btn btn-default" href="mailto:jaslloyd57@gmail.com"><i className="fa fa-envelope fa-2x" /></a>
+          <a id="linkedin-button" className="btn btn-default" href="https://www.linkedin.com/in/jason-lloyd/"><i className="fa fa-linkedin fa-2x" /></a>
         </div>
       </div>
       <div className="row justify-content-center">
@@ -15,7 +15,7 @@ const HeadSection = () => (
           <h1>My Portfolio</h1>
           <OrangeHR />
           <img alt="profilePic" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAMAAQDGAAwAAQAAAAAAAAyaAAAAJGE0MmRlNDMzLTc0YTYtNDZlOS05MzQ5LWEzYmIxOTE4NmVhZg.jpg" className="img--profileimg--size" />
-          <p className="mt-4">My name is Jason, I am currently working as a Senior Software Enginner at IBM building Full stack applications. I have worked with clients creating projects with Angular 2/4, React and VueJS. Here you a find collection of personal and client projects.</p>
+          <p className="mt-4">My name is Jason, I am currently working as a Senior Software Enginner at IBM building Full stack applications. I have worked with clients creating projects with Angular 2+, React and VueJS. Here you a find collection of personal and client projects.</p>
           <a href="[insert_later]" target="_blank" className="btn btn-lg btn-success btn-outline-success mr-2">Hire me</a>
           <button data-toggle="modal" data-target="#myModal" className="btn btn-lg btn-info btn-outline-info">Need a website?</button>
           <div className="btn--downarrow--margin">
